36015045f21eb6b16a60f782e08c2d368ae4359b
UlisesNA <isc_navor.u@tesvb.edu.mx>
2018-10-07 10:29:50 -0500

    Actualizacion
    Template

--- AppData/Config/Autoload.php
+++ AppData/Config/Autoload.php
@@ -1,16 +1,23 @@
-<?php namespace AppData\Config;
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lenovo
+ * Date: 17/09/2018
+ * Time: 08:01 PM
+ */
+namespace AppData\Config;
 
-  class Autoload
-  {
+
+class Autoload
+{
     public static function run()
     {
-      spl_autoload_register(function($class)
-      {
-          $ruta=str_replace("\\",'/', $class).".php";
-          //echo $ruta;
-          include_once($ruta);
-      });
-    }
-  }
+        spl_autoload_register(function ($class)
+        {
+            $ruta=str_replace("\\",'/',$class).'.php';
+            //echo $ruta;
+            require_once ($ruta);
+        });
 
-?>
+    }
+}
--- AppData/Config/Enrutador.php
+++ AppData/Config/Enrutador.php
@@ -1,36 +1,50 @@
-<?php namespace AppData\Config;
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lenovo
+ * Date: 17/09/2018
+ * Time: 08:01 PM
+ */s
 
-	class Enrutador
-	{
+namespace AppData\Config;
 
-		public static function run(Request $request)
-		{
-			$controlador = $request->getControlador() . "Controller";
-			$ruta = ROOT ."AppData" . DS . "Controllers" . DS . $controlador . ".php";
-			$metodo = $request->getMetodo();
-			$argumento = $request->getArgumento();
-			if (is_readable($ruta)) 
-			{
-				$mostrar = "AppData\\Controllers\\" . $controlador;
-				$controlador = new $mostrar;
-				if (!isset($argumento)) 
-				{
-					$datos=call_user_func(array($controlador, $metodo));
-				}
-				else
-				{
-					if(is_callable(array($controlador,$metodo)))
-					{
-						$datos=call_user_func_array(array($controlador, $metodo), $argumento);
-					}
-				}
-			}
-			$ruta = ROOT . "Views" . DS . $request->getControlador() . DS . $request->getMetodo().".php";
-			if(is_readable($ruta))
-				require_once ($ruta);
-			else
-				echo "no se puede cargar la vista";
-		} 
-	}
-	
-?>
+
+class Enrutador
+{
+    public static function run(Request $request)
+    {
+        $controlador = $request->getControlador() . "Controller";
+        $ruta = ROOT . "AppData" . DS . "Controller" . DS . $controlador . ".php";
+
+        $metodo = $request->getMetodo();
+        $argumento = $request->getArgumento();
+
+        if (is_readable($ruta)) {
+            require_once($ruta);
+            $mostrar = "AppData\\Controller\\" . $controlador;
+            $controlador = new $mostrar;
+            if (!isset($argumento)) {
+                $datos = call_user_func(array($controlador, $metodo));
+            } else {
+                $datos = call_user_func(array($controlador, $metodo), $argumento);
+            }
+        }
+
+        if ($request->getMetodo() != "modificar") {
+            $ruta = ROOT . "Views" . DS . $request->getControlador() . DS . $request->getMetodo() . ".php";
+
+            if (is_readable($ruta))
+                require_once($ruta);
+            else
+                if ($request->getMetodo() == "eliminar" || $request->getMetodo() == "actualizar" || $request->getMetodo() == "crear") {
+                    $ruta = ROOT . "Views" . DS . $request->getControlador() . DS . "tabla" . ".php";
+                    if (is_readable($ruta))
+                        require_once($ruta);
+                    else
+                        echo "Esta página no existe";
+                }
+        } else {
+            echo(json_encode(mysqli_fetch_assoc($datos)));
+        }
+    }
+}
